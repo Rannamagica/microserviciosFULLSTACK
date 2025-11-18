84f64cf692eb257b03949e5ee7a245c2f46f577b
JoseArgz8658 <fe.arriagadag@duocuc.cl>
2025-11-18 15:01:34 -0300

    microservicios -- inicio
    micro servicio de register , login , usuario, posteo y membresia

---
+++ posteo/src/test/java/com/fullstack/posteo/PosteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.posteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
